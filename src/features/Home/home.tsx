85036286e96971acafc04c734f3b5f6c071a00cd
Marcin Izdebski <mizdebski123@gmail.com>
2024-07-29 22:41:01 +0200

    questions section added

--- src/features/Home/home.tsx
+++ src/features/Home/home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from './header';
 import { TopItemsSection } from './topItemsSection';
+import { QuestionsSection } from './questionsSection';
 
 export const Home = () => {
     return (
         <main className='border border-black max-w-[1232px] m-auto'>
             <Header />
             <TopItemsSection />
+            <QuestionsSection/>
         </main>
     );
 };
