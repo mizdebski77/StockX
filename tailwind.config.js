b7e7dcb3087d1f5bbcffb82aa1cd392b056545dc
Marcin Izdebski <mizdebski123@gmail.com>
2024-07-29 20:35:23 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      green: "#006340",
+      black: "#2D473E",
+      grey: '#f56651',
+      white: '#f7f7f7',
+    },
+  },
+  plugins: [],
+}
+
