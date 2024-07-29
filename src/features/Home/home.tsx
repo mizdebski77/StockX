d10abf9ba8fad87a99c7d91c61bbe16a314bcd27
Marcin Izdebski <mizdebski123@gmail.com>
2024-07-29 21:59:39 +0200

    Top section added

--- src/features/Home/home.tsx
+++ src/features/Home/home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './header';
+import { TopItemsSection } from './topItemsSection';
 
 export const Home = () => {
     return (
         <main className='border border-black'>
             <Header />
+            <TopItemsSection />
         </main>
     );
 };
---
+++ src/features/Home/topItemsSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const TopItemsSection = () => {
+    return (
+        <section className='mt-12'>
+            <h2 className='text-5xl text-center font-bold text-darkGreen leading-[55px]'>Top Tech Mystery Box<p>
+                at Unbeatable Prices
+            </p>
+            </h2>
+            <p className='text-xl text-[#006340] text-center font-medium mt-2'>Fast shipping | Easy returns | Special discounts for students</p>
+
+        </section>
+    );
+};
