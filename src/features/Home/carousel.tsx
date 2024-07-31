abc2e56ce7ea42a8a93ad4a71d8d2ef07ca284ab
Marcin Izdebski <mizdebski123@gmail.com>
2024-07-31 09:23:53 +0200

    Images to carousel added

---
+++ src/features/Home/carousel.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import img1 from '../../common/Images/Header/Carousel/img1.svg';
+import img2 from '../../common/Images/Header/Carousel/img2.svg';
+import img3 from '../../common/Images/Header/Carousel/img3.svg';
+import img4 from '../../common/Images/Header/Carousel/img4.svg';
+
+const Carousel = () => {
+    const [mainImg, setMainImg] = useState(img1);
+    const thumbnails = [img1, img2, img3, img4];
+
+    return (
+
+        <div className="flex flex-col items-center">
+            <div className="main-image mb-4">
+                <img src={mainImg} alt='header 1' className='max-w-[481px] w-full h-[644px]' />
+            </div>
+            <div className="thumbnails flex space-x-2">
+                {thumbnails.map((thumbnail, index) => (
+                    <img
+                        key={index}
+                        src={thumbnail}
+                        alt={`Thumbnail ${index + 1}`}
+                        className="w-20 h-20 object-cover cursor-pointer border-2 border-transparent hover:border-blue-500"
+                        onClick={() => setMainImg(thumbnail)}
+                    />
+                ))}
+            </div>
+        </div>
+
+    );
+};
+
+export default Carousel;
--- src/features/Home/header.tsx
+++ src/features/Home/header.tsx
@@ -4,37 +4,19 @@ import mcAfee from '../../common/Images/mcAfee.svg';
 import truste from '../../common/Images/truste.svg';
 import header1 from '../../common/Images/Header/header1.svg';
 import price from '../../common/Images/Header/price.svg';
-import xd1 from '../../common/Images/bit.svg';
-import xd2 from '../../common/Images/mcAfee.svg';
-import xd3 from '../../common/Images/bit.svg';
+import Carousel from './carousel';
+
 
 export const Header = () => {
-    const [mainImg, setMainImg] = useState(xd1);
-    const thumbnails = [xd1, xd2, xd3];
 
     return (
         <header className='flex justify-center gap-[21px] items-center ml-[27px] '>
+            <Carousel/>
             <div className='relative'>
                 <img src={price} alt='price' className='absolute top-[-45px] right-[-44px] max-w-60 w-full' />
 
-                <div className="flex flex-col items-center">
-                    <div className="main-image mb-4">
-                        <img src={mainImg} alt='header 1' className='max-w-[481px] w-full h-[644px]' />
-                    </div>
-                    <div className="thumbnails flex space-x-2">
-                        {thumbnails.map((thumbnail, index) => (
-                            <img
-                                key={index}
-                                src={thumbnail}
-                                alt={`Thumbnail ${index + 1}`}
-                                className="w-20 h-20 object-cover cursor-pointer border-2 border-transparent hover:border-blue-500"
-                                onClick={() => setMainImg(thumbnail)}
-                            />
-                        ))}
-                    </div>
-                </div>
-
             </div>
+            
             <div className='max-w-[687px] w-full'>
                 <div className='text-darkGreen'>
                     <h1 className='text-[52px] font-extrabold leading-[55px] mb-2'>Chance to win a StockX <p className='m-0'>Mystery Box today!</p></h1>
